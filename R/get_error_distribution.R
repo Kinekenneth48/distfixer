3cd0d5b66cfe385affdf45f129cada0f5b830106
Kinekenneth48 <Kinekenneth48@gmail.com>
2023-03-13 10:57:37 -0600

    modified function to allow users to pass other arguments to fitdistrplus::fitdist

--- R/get_error_distribution.R
+++ R/get_error_distribution.R
@@ -7,14 +7,20 @@
 #'  model of class "ranger" when random forest if fitted, "ksvm"
 #'  when support vector regression is fitted, and "gbm.object" when gradient
 #'  boosting machine is fitted.
+#' @param distr A character string that represent the distribution to fit. 
+#' Default is "norm" for normal distribution. See fitdistrplus::fitdist for 
+#' string names for other distributions. 
 #' @param label Response/dependent variable name in the train_data.
-#' @return Normal distribution parameters.
+#' @param ... Other arguments to send to the distribution functions
+#'  fitdistrplus::fitdist
+#' @return An object of class "fitdist", which includes the distribution names
+#'  and parameter estimates.
 #' @seealso
 #' [fitdistrplus::fitdist()], [distfixer::fit_model()].
 #' @rdname get_error_distribution
 #' @export
 #' @importFrom fitdistrplus fitdist
-get_error_distribution <- function(train_data, fitted_model, label) {
+get_error_distribution <- function(train_data, fitted_model, distr = "norm", label, ...) {
   # get class of model
   model_type <- class(fitted_model)
 
@@ -23,22 +29,22 @@ get_error_distribution <- function(train_data, fitted_model, label) {
     "ranger" = {
       res <- as.numeric(train_data[[label]]) -
         predict(fitted_model, train_data)[["predictions"]]
-      fit_norm <- fitdistrplus::fitdist(res, "norm")
+      fit_norm <- fitdistrplus::fitdist(res, distr = distr)
     },
     "ksvm" = {
       res <- as.numeric(train_data[[label]]) - predict(fitted_model, train_data)
-      fit_norm <- fitdistrplus::fitdist(res, "norm")
+      fit_norm <- fitdistrplus::fitdist(res, distr = distr)
     },
     "gbm" = {
       res <- as.numeric(train_data[[label]]) - predict(fitted_model, train_data)
-      fit_norm <- fitdistrplus::fitdist(res, "norm")
+      fit_norm <- fitdistrplus::fitdist(res, distr = distr)
     },
     stop(paste("Unknown method:", model_type))
   )
 
+
   return(error_dist = list(
-    distribution = error_distr$distname,
-    parameter_mean = error_distr$estimate[["mean"]],
-    parameter_sd = error_distr$estimate[["sd"]]
+    distribution = error_distr[["distname"]],
+    estimate = error_distr[["estimate"]]
   ))
 }
--- tests/testthat/test_get_error_distribution.R
+++ tests/testthat/test_get_error_distribution.R
@@ -1,22 +1,22 @@
-testthat::test_that("Test 1: get_error_distribution function", {
+testthat::test_that("Test 1: get_error_distribution function: mean", {
   set.seed(1232)
   expected <- get_error_distribution(
-    train_data = data, label = "y",
+    train_data = data, label = "y", distr = "norm",
     fitted_model = model
   )
-  actual <- round(0.0086413837, 4)
-  expected <- round(expected$parameter_mean, 4)
+  actual <- as.vector(round(0.0086413837, 4))
+  expected <- as.vector(round(expected$estimate[1], 4))
   expect_equal(expected, actual)
 })
 
 
-testthat::test_that("Test 1: get_error_distribution function", {
+testthat::test_that("Test 1: get_error_distribution function: sd", {
   set.seed(1232)
   expected <- get_error_distribution(
-    train_data = data, label = "y",
+    train_data = data, label = "y", distr = "norm",
     fitted_model = model
   )
-  actual <- round(2.243325, 2)
-  expected <- round(expected$parameter_sd, 2)
+  actual <- as.vector(round(2.243325, 2))
+  expected <- as.vector(round(expected$estimate[2], 2))
   expect_equal(expected, actual)
 })
